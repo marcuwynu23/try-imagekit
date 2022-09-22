65e51d7c026854ea375c90595b82b760e8742e68
marcuwynu23 <marcuwynu23@gmail.com>
2022-09-23 05:28:24 +0800

    .

--- imagekit.js
+++ imagekit.js
@@ -1,18 +1,12 @@
-
+require("dotenv").config()
 const Imagekit = require('imagekit');
 
 
-const WEBHOOK_SECRET = 'whsec_1h6Qus2opjCvC838IxxnF9l7oCfu+DdT'; // Copy from Imagekit dashboard
-const WEBHOOK_EXPIRY_DURATION = 300 * 1000; // 300 seconds for example
 
 const imagekit = new Imagekit({
-	publicKey: 'public_YU14XW7TeRiSqRT6g4bvLCi5ItY=',
-	urlEndpoint: 'https://ik.imagekit.io/kalapatid',
-	privateKey: 'private_WMRcv/JAsTN1KvN/GVoXCeJGnv4=',
+	publicKey: process.env.PUBLIC_KEY,
+	urlEndpoint: process.env.END_POINT,
+	privateKey: process.env.PRIVATE_KEY,
 })
-
-
-
-
 
 module.exports = imagekit;
